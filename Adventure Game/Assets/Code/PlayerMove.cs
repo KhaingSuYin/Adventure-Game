35917ef6b6cca1a8b7d6eeec50f3e0450a878320
Alice Khaing <ksy243@nyu.edu>
2021-11-11 12:02:45 -0500

    Complete mazes and keys

--- Adventure Game/Assets/Code/PlayerMove.cs
+++ Adventure Game/Assets/Code/PlayerMove.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class PlayerMove : MonoBehaviour
 {
     // Start is called before the first frame update
     NavMeshAgent agent;
     Camera mainCam;
-    public Transform spawnPoint;
-    public GameObject bulletPrefab;
-    float startTime;
+    public GameObject[] keys;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         mainCam = Camera.main;
         agent.transform.position = new Vector3(PublicVars.posX, PublicVars.posY, PublicVars.posZ);
+        keys = GameObject.FindGameObjectsWithTag("Key");
+        keys = keys.OrderBy(p => p.name).ToArray();
         if (PublicVars.L3keys > 0)
         {
             for (int i = 0; i < PublicVars.L3keys; ++i)
             {
-                Destroy(GameObject.FindGameObjectWithTag("Key" + i));
+                Destroy(keys[i]);
             }
         }
     }
@@ -45,11 +46,12 @@ public class PlayerMove : MonoBehaviour
         if (other.gameObject.CompareTag("Key"))
         {
             Destroy(other.gameObject);
+            PublicVars.L3keys++;
             PublicVars.hasKey = true;
             PublicVars.posX = agent.transform.position.x;
             PublicVars.posY = agent.transform.position.y;
             PublicVars.posZ = agent.transform.position.z;
-            SceneManager.LoadScene("Maze", LoadSceneMode.Single);
+            SceneManager.LoadScene("Maze"+ PublicVars.L3keys, LoadSceneMode.Single);
         }
     }
 }
