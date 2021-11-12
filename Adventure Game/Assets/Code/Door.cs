dcf8f246a0dc91ac74e4ef7be03e0f5331261669
Hiroki39 <zhenghongyi2000@hotmail.com>
2021-11-12 01:44:05 -0500

    bug fixes

--- Adventure Game/Assets/Code/Door.cs
+++ Adventure Game/Assets/Code/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
@@ -26,6 +27,6 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Adventure Game/Assets/Code/PublicVars.cs
+++ Adventure Game/Assets/Code/PublicVars.cs
@@ -1,9 +1,9 @@
+using UnityEngine;
+
 public static class PublicVars
 {
     public static bool hasKey = false;
-    public static float posX = 32.5f;
-    public static float posY = 0.7f;
-    public static float posZ = 5.3f;
+    public static Vector3 position = new Vector3(32.5f, 0.7f, 5.3f);
     public static bool paused = false;
     public static int L3keys = 0;
 }
